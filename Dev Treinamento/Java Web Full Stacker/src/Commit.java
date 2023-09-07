a14dd2b5b2828db5d81c505e82829230902d9c32
Valdiran <valdiran47@gmail.com>
2023-09-07 18:01:49 -0300

    comitando

--- Dev Treinamento/Java Web Full Stacker/src/Commit.java
+++ Dev Treinamento/Java Web Full Stacker/src/Commit.java
@@ -1,5 +1,6 @@
 public class Commit {
     public static void main(String[] args) {
         System.out.println("Olá Mundo");
+        System.out.println("Estou aprendendo a programar em java");
     }
 }
