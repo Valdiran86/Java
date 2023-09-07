4845d66d44a3bcb1d1cff7438dc80881024eb42c
Valdiran <valdiran47@gmail.com>
2023-09-07 18:06:15 -0300

    subindo arquivos para o git Hube

--- Dev Treinamento/Java Web Full Stacker/src/Commit.java
+++ Dev Treinamento/Java Web Full Stacker/src/Commit.java
@@ -2,5 +2,7 @@ public class Commit {
     public static void main(String[] args) {
         System.out.println("Olá Mundo");
         System.out.println("Estou aprendendo a programar em java");
+        System.out.println("Aprendendo comandos no Git Hube");
+        System.out.println("Comandos do git ");
     }
 }
